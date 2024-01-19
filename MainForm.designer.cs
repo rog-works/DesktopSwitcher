25b618224690f3cc3ea306d1603f28dd76ec17fd
rog-works <rogangelf@gmail.com>
2024-01-19 11:15:24 +0900

    fix: デザイナーの更新

--- MainForm.designer.cs
+++ MainForm.designer.cs
@@ -31,20 +31,20 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.dataGridViewShortcut = new System.Windows.Forms.DataGridView();
-            this.shortcutGroup = new System.Windows.Forms.GroupBox();
-            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
-            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.desktopNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.shiftDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.ctrlDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.altDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
             this.keyNumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.shortcutGroup = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewShortcut)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.shortcutGroup.SuspendLayout();
             this.menuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridViewShortcut
@@ -67,45 +67,6 @@
             this.dataGridViewShortcut.RowTemplate.Height = 21;
             this.dataGridViewShortcut.Size = new System.Drawing.Size(569, 168);
             this.dataGridViewShortcut.TabIndex = 2;
-            // 
-            // shortcutGroup
-            // 
-            this.shortcutGroup.Controls.Add(this.dataGridViewShortcut);
-            this.shortcutGroup.Location = new System.Drawing.Point(12, 27);
-            this.shortcutGroup.Name = "shortcutGroup";
-            this.shortcutGroup.Size = new System.Drawing.Size(575, 186);
-            this.shortcutGroup.TabIndex = 3;
-            this.shortcutGroup.TabStop = false;
-            this.shortcutGroup.Text = "ショートカット";
-            // 
-            // menuStrip1
-            // 
-            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.fileToolStripMenuItem});
-            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
-            this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(600, 24);
-            this.menuStrip1.TabIndex = 4;
-            this.menuStrip1.Text = "menuStrip1";
-            // 
-            // fileToolStripMenuItem
-            // 
-            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.quitToolStripMenuItem});
-            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
-            this.fileToolStripMenuItem.Text = "ファイル";
-            // 
-            // quitToolStripMenuItem
-            // 
-            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
-            this.quitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
-            this.quitToolStripMenuItem.Text = "終了";
-            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
-            // 
-            // bindingSource1
-            // 
-            this.bindingSource1.DataSource = typeof(DesktopSwitcher.ShortcutDef);
             // 
             // desktopNameDataGridViewTextBoxColumn
             // 
@@ -142,6 +103,45 @@
             this.keyNumberDataGridViewTextBoxColumn.Name = "keyNumberDataGridViewTextBoxColumn";
             this.keyNumberDataGridViewTextBoxColumn.ReadOnly = true;
             // 
+            // bindingSource1
+            // 
+            this.bindingSource1.DataSource = typeof(DesktopSwitcher.ShortcutDef);
+            // 
+            // shortcutGroup
+            // 
+            this.shortcutGroup.Controls.Add(this.dataGridViewShortcut);
+            this.shortcutGroup.Location = new System.Drawing.Point(12, 27);
+            this.shortcutGroup.Name = "shortcutGroup";
+            this.shortcutGroup.Size = new System.Drawing.Size(575, 186);
+            this.shortcutGroup.TabIndex = 3;
+            this.shortcutGroup.TabStop = false;
+            this.shortcutGroup.Text = "ショートカット";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(600, 24);
+            this.menuStrip1.TabIndex = 4;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.fileToolStripMenuItem.Text = "ファイル";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
+            this.quitToolStripMenuItem.Text = "終了";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
@@ -158,10 +158,10 @@
             this.Text = "DesktopSwitcher";
             this.VisibleChanged += new System.EventHandler(this.MainForm_VisibleChanged);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewShortcut)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.shortcutGroup.ResumeLayout(false);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
